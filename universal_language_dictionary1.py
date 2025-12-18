4ddf2213604f135b563d1b3cd9edaa25d33a03d1
Kpongo Emmanuel <Emmanuel.Kpongo@Binghamuni.edu.ng>
2025-12-18 18:11:58 +0100

    Emmanuel updated the code

--- universal_language_dictionary1.py
+++ universal_language_dictionary1.py
@@ -6,7 +6,7 @@ print(Languages)
 
 language_choice = input('Which language do you want to use?')
 if language_choice == 'a':
-        print('you have chosen "Tiv" language, you may now choose from the following words to translate')
+        print('you have chosen "Tiv" language, you may now choose from the following words to translate:')
         print(eng_words)
         Tiv_dict = {'come': 'vah', 'take': 'ngohol', 'see': 'nenge', 'go': 'yem', 'eat': 'ya', 'house': 'yah',
                     'sleep': 'yav', 'walk': 'zande', 'work': 'tom', 'play': 'numbe', 'ask': 'pine', 'food': 'kwaghyan',
@@ -19,20 +19,20 @@ if language_choice == 'a':
             else:
                  print("please choose from the list of words")
 if language_choice == 'b':
-    print('you have chosen "Igbo" language, you may now choose from the following words to translate.') #this is for Treasure
+    print('you have chosen "Igbo" language, you may now choose from the following words to translate:') #this is for Treasure
 #---> Print your list of english words on this line (you can delete this)
 #then yea you can paste your code here->
 
 if language_choice == 'c':
-    print('you have chosen "Jenjo" language, you may now choose from the following words to translate') #this is for Pheeyamila
+    print('you have chosen "Jenjo" language, you may now choose from the following words to translate:') #this is for Pheeyamila
 #---> Print your list of english words on this line (you can delete this)
 #then yea you can paste your code here->
 
 if language_choice == 'd':
-    print('you have chosen "Your_language" language, you may now choose from the following words to translate')  # this is for Uriel, please replace 'Your_language' with your language for the dictionary
+    print('you have chosen "Your_language" language, you may now choose from the following words to translate:')  # this is for Uriel, please replace 'Your_language' with your language for the dictionary
     # ---> Print your list of english words on this line (you can delete this)
     # then yea you can paste your code here->
 
 if language_choice == 'e':
-    print('you have chosen "Your_language" language, you may now choose from the following words to translate')  # this is for Alero, please replace 'Your_language' with your language for the dictionary
+    print('you have chosen "Your_language" language, you may now choose from the following words to translate:')  # this is for Alero, please replace 'Your_language' with your language for the dictionary
     # ---> Print your list of english words on this line (you can delete this)
